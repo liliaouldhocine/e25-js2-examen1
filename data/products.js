805d02d333f7718094b2fa6721dd4194e1715ce6
Lilia Ould Hocine <liliaohl@gmail.com>
2025-05-22 10:01:32 -0400

    first commit

---
+++ data/products.js
@@ -0,0 +1,182 @@
+export const produits = [
+  {
+    nom: "Confidential Saffron Lazuli",
+    maison: "Carolina Herrera",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2023/04/Carolina-Herrera-Saffron-Lazuli-edt.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "329.99",
+    description:
+      "Top Notes: Saffron &amp; Blackcurrant\r\n\r\nHeart Notes: Rose &amp; Orris\r\n\r\nBase Notes: Gaiac Wood, Vanilla &amp; Leather",
+    id: 1742596869,
+  },
+  {
+    nom: "L - Floral Chypre",
+    maison: "Clive Christian",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/clive-christian-L-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "309.95",
+    description:
+      "L for Women Floral Chypre With Rich Patchouli by Clive Christian is a Chypre Floral fragrance for women. L for Women Floral Chypre With Rich Patchouli was launched in 2017. The nose behind this fragrance is Angela Stavrevska. Top note is Flowers; middle notes are Rose and Jasmine Sambac; base note is Patchouli.",
+    id: 1742627684,
+  },
+  {
+    nom: "Luna",
+    maison: "Kemi Blending Magic",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2023/05/kemi-luna-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "439.95",
+    description:
+      "Top Notes: Lemon, Mandarin, Apple, Mango, Herbal Notes, Black Currant,\r\nApricot, Incense, Spicy Notes\r\n\r\nHeart Notes: Cedar Wood, Jasmine, Rose, Iris, Russian Leather\r\n\r\nBase Notes: Amber, Musk, Vanilla Madagascar",
+    id: 1742627729,
+  },
+  {
+    nom: "For Your Love",
+    maison: "Mizensir",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/mizensir-for-your-love-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "345.00",
+    description:
+      "Top Notes: RASPBERRY NOTE, EXALTONE\r\n\r\nHeart Notes: CACHALOX\r\n\r\nBase Notes: ESSENCE OF BENZOIN, Heart OF PATCHOULI",
+    id: 1742627779,
+  },
+  {
+    nom: "Pardon Eau de Parfum",
+    maison: "Nasomatto",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/nasomatto-pardon-1-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "270.00",
+    description:
+      "Top Notes:\r\nAldehyde, Cinnamon, Mandarin\r\n\r\nHeart Notes: Magnolia\r\n\r\nBase Notes:\r\nAgarwood, Black chocolate, Castoreum, Cedar, Musk, Sandalwood, Smoke, Tonka, Vanilla",
+    id: 1742627819,
+  },
+  {
+    nom: "Moonlight in Heaven",
+    maison: "By Kilian",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/11/kilian-moonlight-in-heaven-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "499.99",
+    description:
+      "Refill Kit includes:\r\n50ml/1.7 O.Z filled bottle; empty 7.5ml/ 1 OZ spray bottle; funnel; Dropper; extra sprayer ",
+    id: 1742627877,
+  },
+  {
+    nom: "Bal D&#039;Afrique",
+    maison: "Byredo",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/byredo-bal-dafrique.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "309.95",
+    description:
+      "TOP NOTES: African Marigold, Bergamot, Buchu\r\n\r\nHEART NOTES: Cyclamen, Violet\r\n\r\nBASE NOTES: Moroccan Cedarwood, Vetiver",
+    id: 1742627924,
+  },
+  {
+    nom: "Duchessa",
+    maison: "Gritti",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2023/06/Duchessa-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "375.00",
+    description:
+      "TOP NOTES\r\nBlack cherry, Bitter orange, Saffron flower\r\n\r\nHEART NOTES\r\nJasmine, Cloves, Cocoa\r\n\r\nBASE NOTES\r\nIris, Almond, Brown sugar",
+    id: 1742627963,
+  },
+  {
+    nom: "Santal Royal",
+    maison: "Guerlain",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/guerlain-santal-royal-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "285.00",
+    description:
+      "Santal Royal by Guerlain is a Oriental Woody fragrance for women and men. This is a new fragrance. Santal Royal was launched in 2024. The nose behind this fragrance is Thierry Wasser.",
+    id: 1742628029,
+  },
+  {
+    nom: "Silky Woods",
+    maison: "Goldfield & Banks",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2024/04/goldfield-and-banks-silky-woods-600x600.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "339.99",
+    description:
+      "Main Notes:\r\nAgarwood Tropical Australia, Cinnamon Ceylon, Tobacco Leaves, Ylang Ylang Madagascar, Vanilla Tahiti, Sandalwood Australia, Incense",
+    id: 1742628067,
+  },
+  {
+    nom: "Velvet Tonka",
+    maison: "BDK Parfums",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/10/bdk-velvet-tonka-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "399.95",
+    description:
+      "Top Notes: Orange blossom, Almond\r\n\r\nHeart Notes: Rose absolute, Tobacco absolute\r\n\r\nBase Notes, Tonka, Vanilla, Amyris wood essence, Amber woods",
+    id: 1742628109,
+  },
+  {
+    nom: "Imitation (M)",
+    maison: "Amouage",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/07/amouage-imitations-50ml-600x600.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "295.00",
+    description:
+      "Top Notes: Cedrat, Nutmeg, Black Pepper.\r\n\r\nHeart Notes: Turkish Rose, Orris, Violet.\r\n\r\nBase Notes: Myrrh, Leather, Vetiver, Patchouli, Castoreum.",
+    id: 1742628161,
+  },
+  {
+    nom: "Spring Fling",
+    maison: "Bond No.9",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2023/06/bond-no9-spring-fling-1024x1024.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    prix: "329.95",
+    description:
+      "Main Notes:\r\nLily of the Valley, Passion Flower, Honeysuckle, Jasmin, Freesia, Amber, and Musk",
+    id: 1742628197,
+  },
+  {
+    nom: "Vibrato",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2023/03/sospiro-vibrato-2-600x600.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    maison: "Sospiro",
+    prix: "375.00",
+    description:
+      "Vibrato by Sospiro Perfumes is a fragrance for women and men. This is a new fragrance. Vibrato was launched in 2022. The nose behind this fragrance is Christian Provenzano. Top notes are Grapefruit, Bergamot, Jasmine and Magnolia; middle notes are Ginger, Herbal Notes and Powdery Notes; base notes are Musk, Cedar, Amber, Patchouli and Orris Root.",
+    id: 1742629418,
+  },
+  {
+    nom: "Megalium",
+    // image:
+    //   "https://shoperfumes.ca/wp-content/uploads/2022/07/carner-barcelona-megalium-600x600.jpg",
+    image:
+      "https://images.unsplash.com/photo-1707899997123-226a3f57aa32?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    maison: "Carner Barcelona",
+    prix: "215.95",
+    description:
+      "Top Notes\r\nRoyal cinnamon essence from Laos, Cinnamon tree leaves from Sri Lanka, Mandarin integral extract, Calamus essence India\r\n\r\nHeart Notes\r\nNutmeg essence from Moluccas, Indonesan Pimento Berry essence, Indian White pepper absolute, Bulgarian rose extract\r\n\r\nBase Notes\r\nOlibanum extract Somalia, Ethiopian Myrrh extract, Yemen&iacute; Opoponax extract, Styrax resin",
+    id: 1742671900,
+  },
+];
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import "./assets/styles/styles.scss";
+import "./main.scss";
